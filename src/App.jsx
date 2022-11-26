4c53a6a5708d47abb286012bd979a1dda35b9b06
Filip Drábek <drabekfilip@seznam.cz>
2022-11-26 14:36:16 +0100

    creating new react project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import "./Scss/App.scss";
+import Header from "./Components/Footer";
+import Footer from "./Components/Header";
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+    return(
+      <footer className='footer'>&#169; 2022 Drábek development</footer>
+    )
+  }
